361c504f10db7d26429950d1a23f8b2cd4ab0a23
Vanina Denegri <denegri.vanina@gmail.com>
2025-01-19 23:52:35 -0300

    Limpieza de código

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -5,26 +5,27 @@ import { BsExclamationTriangleFill } from "react-icons/bs";
 import { useFavoritosContext } from "context/Favoritos";
 
 function Favoritos() {
-    const { favorito } = useFavoritosContext();
+    const { favorito: favoritos } = useFavoritosContext();
 
     return (
         <>
-            <Banner img="favoritos" txt="Favoritos"></Banner>
+            <Banner img="favoritos" txt="Favoritos" />
             <section className={styles.container}>
-                {favorito.length === 0 ? (
-                    <h2 className={styles.mensaje}><BsExclamationTriangleFill />No se han seleccionado favoritos</h2>
+                {favoritos.length === 0 ? (
+                    <h2 className={styles.mensaje}>
+                        <BsExclamationTriangleFill />No se han seleccionado favoritos
+                    </h2>
                 ) : (
-                    favorito.map((fav) => (
+                    favoritos.map((video) => (
                         <Video
-                            {...fav}
-                            key={fav.id}
+                            key={video.id}
                             datos={{
-                                titulo: fav.titulo,
-                                categoria: fav.categoria,
-                                imagen: fav.imagen,
-                                link: fav.link,
-                                descripcion: fav.descripcion,
-                                id: fav.id,
+                                titulo: video.titulo,
+                                categoria: video.categoria,
+                                imagen: video.imagen,
+                                link: video.link,
+                                descripcion: video.descripcion,
+                                id: video.id,
                             }}
                         />
                     ))
