3237185957bb9888dc84edb0232e8bb000913b6d
Vanina Denegri <denegri.vanina@gmail.com>
2025-01-19 22:26:47 -0300

    Crea un archivo aparte con el array de videos y arregla el código en el componente Player ya que tomaba los videos de otra parte.

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,20 +1,5 @@
-import { useState } from "react";
-import styles from "./index.module.css";
-import Hero from "components/Hero";
-import Formulario from "components/Formulario";
-import { useFormulario } from "context/FormularioContext";
-import Categoria from "components/Categoría";
-import ModalEditar from "components/ModalEditar";
-import videosData from "data/videos.js";
-
-//ESTAS EN LA RAMA MODALES
-
-function Inicio() {
-    const { mostrarFormulario } = useFormulario();
-    const [ videos, setVideos] = useState(videosData);
-
-/*     //Lista de videos
-    const [videos, setVideos] = useState([{
+const videos = [
+    {
         id: 1,
         titulo: "Rigatoni alla Norma",
         categoria: "Pastas",
@@ -86,7 +71,6 @@ function Inicio() {
         link: "https://www.youtube.com/embed/HVA31cX7EtQ?si=6S-oYBKzLpF3LMsa",
         descripcion: "Receta de ensalada a base de pepinos"
     },
-
     {
         id: 10,
         titulo: "Spaghetti verdi",
@@ -112,95 +96,8 @@ function Inicio() {
         imagen: "https://i.postimg.cc/Px2gG15z/ensalada-04-mariscos.png",
         link: "https://www.youtube.com/embed/mERYTifdTDQ?si=aSx4q3gtR-PG_BU0",
         descripcion: "Receta de ensalada a base de mariscos"
-    }]);
- */
-    //Lista de categorías
-    const categorias = [
-        {
-            tag: "Pastas",
-            color: "#BF0404",
-            sombra: "0px 0px 4px 1px rgba(191, 4, 4, 0.7)"
-        },
-
-        {
-            tag: "Pizzas",
-            color: "#F29F05",
-            sombra: "0px 0px 4px 1px rgba(242, 159, 5, 0.7)"
-        },
-
-        {
-            tag: "Ensaladas",
-            color: "#83A603",
-            sombra: "0px 0px 4px 1px rgba(131, 166, 3, 0.7)"
-        }
-    ]
-
-    //Registrar video
-    const registrarVideo = (video) => {
-        setVideos([...videos, video]);
-    };
-
-    //Eliminar video
-    const eliminarVideo = (id) => {
-        const nuevosVideos = videos.filter((video) => video.id !== id);
-        setVideos(nuevosVideos);
     }
+];
 
-    //Modal - Formulario Editar Video
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [videoAEditar, setVideoAEditar] = useState(null);
+export default videos;
 
-    //Abre el modal y conecta el video a editar
-    const manejarEdicionVideo = (video) => {
-        setVideoAEditar(video);
-        setIsModalOpen(true);
-    };
-
-    //Cerrar el modal
-    const closeModal = () => {
-        setIsModalOpen(false);
-        setVideoAEditar(null);
-    };
-
-    const actualizarVideo = (videoActualizado) => {
-        setVideos(videos.map((v) => (v.id === videoActualizado.id ? videoActualizado : v)));
-        closeModal(); // Cerrar modal después de actualizar
-    };
-
-    return (
-        <div className={styles.container}>
-            <Hero img="home" />
-            <section className={styles.containerContenido}>
-                {mostrarFormulario &&
-                    <Formulario
-                        categorias={categorias.map((categoria) => categoria.tag)}
-                        registrarVideo={registrarVideo}
-                    />
-                }
-
-                {isModalOpen && videoAEditar && (
-                    <ModalEditar
-                        video={videoAEditar}
-                        closeModal={closeModal}
-                        categorias={categorias.map((categoria) => categoria.tag)}
-                        actualizarVideo={actualizarVideo}
-                    />
-                )}
-
-                <div className={styles.containerCategorias}>
-                    {categorias.map((categoria) => (
-                        <Categoria
-                            datos={categoria}
-                            key={categoria.tag}
-                            videos={videos.filter((video) => video.categoria === categoria.tag)}
-                            eliminarVideo={eliminarVideo}
-                            manejarEdicionVideo={manejarEdicionVideo}
-                        />
-                    ))}
-                </div>
-            </section>
-        </div>
-    );
-};
-
-export default Inicio;
--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,30 +1,22 @@
 import Banner from "components/Banner";
 import styles from "./Player.module.css";
 import { useParams } from "react-router-dom";
-import videos from "data/db.json";
 import NotFound from "pages/NotFound";
-import { useEffect, useState } from "react";
+import videos from "data/videos.js";
 
 function Player() {
-    const [video, setVideo] = useState([]);
     const parametros = useParams();
+    const video = videos.find((video)=> video.id === Number(parametros.id));
 
-    useEffect(() => {
-        fetch(`https://my-json-server.typicode.com/VannDennOk/food-flix-api/videos?id=${parametros.id}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setVideo(...data)
-            })
-    }, [])
-
-    // const video = videos.find((video)=> video.id === Number(parametros.id));
     if (!video) return <NotFound />
+
     return (
         <>
             <Banner img="player" txt="Player"></Banner>
 
             <section className={styles.container}>
                 <h2>{video.titulo}</h2>
+                <h2>MODALES</h2>
                 <iframe
                     src={video.link}
                     title={video.titulo}
